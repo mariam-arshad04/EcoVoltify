bb9d3ed2baa2074a3b471323354b46588a001fdd
mariam-arshad04 <146752574+mariam-arshad04@users.noreply.github.com>
2025-05-18 16:06:21 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,34 +1,33 @@
 <?php
-    require 'vendor/autoload.php';
-    require('stripe-php-master/init.php');
-    require('stripe_keys.php');
-    \Stripe\Stripe::setApiKey($secret_key);
+    
+    session_start(); // ✅ Required to access $_SESSION
+    use Stripe\Terminal\Location;
 
-// use Stripe\Terminal\Location;
+    require __DIR__ . "/vendor/autoload.php";
+    $stripe_secret_key = "sk_test_51RNbXGRdy99MB75kg4Dz8YqVSi5xRaNRVOtoafIO46qgDLO0Zp95YzCOu4heUhEjcueaaiZz9j8Cd2NZBZsewqa500ZzwU3ApW";
+    \Stripe\Stripe::setApiKey($stripe_secret_key);
 
-//     require __DIR__ . "/vendor/autoload.php";
-//     
-//     
 
-//     $checkout_session = \Stripe\Checkout\Session::create([
-//         "mode" => "payment",
-//         "success_url" => "http://localhost/EcoVoltify/success.php",
-//         "line_items" => [
-//             [
-//                 "quantity" => 1,
-//                 "price_data" => [
-//                     "currency" => "usd",
-//                     "unit_amount" => $_SESSION['finaltotal']*100,
-//                     "product_data" => [
-//                         "name" => $_SESSION['product_name'],
+    $checkout_session = \Stripe\Checkout\Session::create([
+        "mode" => "payment",
+        "success_url" => "http://localhost/EcoVoltify/homepage.php",
+        "line_items" => [
+            [
+                "quantity" => 1,
+                "price_data" => [
+                    "currency" => "usd",
+                    "unit_amount" => $_SESSION['order_total_with_donation']*100,
+                    "product_data" => [
+                        "name" => "Total Price",
 
-//                     ]
-//                 ]
-//             ]
-//         ]
+                    ]
+                ]
+            ]
+        ]
 
-//     ]);
-//     http_response_code(303);
-//     header("Location: " . $checkout_session->url);
+    ]);
+    http_response_code(303);
+    header("Location: " . $checkout_session->url);
+
 
  ?>
